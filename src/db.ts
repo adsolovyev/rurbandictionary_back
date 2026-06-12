3a15a7b8fbf7c6208218c7d5ab235ff6826a2b37
Andrey Solovyev <sadako.inc@gmail.com>
2026-06-12 17:38:47 +0300

    db upd

--- src/db.ts
+++ src/db.ts
@@ -3,12 +3,19 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
+const isProduction = !!process.env.DATABASE_URL;
 
-export default pool;
+const pool = new Pool(
+  isProduction
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false },
+      }
+    : {
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT),
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+      }
+);
