1b78e3d700e7d49565a9451700c7733ea7ed6a53
Andrey Solovyev <sadako.inc@gmail.com>
2026-06-13 20:08:49 +0300

    api change

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -20,7 +20,12 @@ router.get('/google/callback', passport.authenticate('google', { failureRedirect
     process.env.JWT_SECRET!,
     { expiresIn: '7d' }
   );
-  res.cookie('token', token, { httpOnly: true, sameSite: 'lax', maxAge: 7 * 24 * 60 * 60 * 1000 });
+  res.cookie('token', token, {
+  httpOnly: true,
+  secure: true,
+  sameSite: 'lax',
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+});
   const redirectUrl = process.env.FRONTEND_URL || 'http://localhost:5173';
   res.redirect(redirectUrl);
 });
